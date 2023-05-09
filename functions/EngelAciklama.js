138b93a196120c9760f5535cc2eb0f3a88f589ff
Asiye <68599181+asiyeyildirim@users.noreply.github.com>
2023-05-09 11:32:13 +0300

    Add files via upload

---
+++ functions/EngelAciklama.js
@@ -0,0 +1,22 @@
+
+export const  AciklamaGoster = () =>{
+    var engeldurum = document.getElementById("engel").value;
+    var aciklamaDiv = document.getElementById("aciklamaDiv");
+    var icerik = document.getElementsByClassName("icerik");
+    if (engeldurum == "var") {
+      
+        icerik.innerHTML = engeldurum;
+        aciklamaDiv.style.display = "block";
+
+    } else {
+        icerik.innerHTML = engeldurum;
+        aciklamaDiv.style.display = "none";
+        icerik.style.display = "none";
+        
+    }
+
+
+
+}
+
+
